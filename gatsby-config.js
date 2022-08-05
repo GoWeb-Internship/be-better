deb882ee2895d469279036d93760f4274236fc13
Korotenko Alex <alexey.korotenko90@gmail.com>
2022-08-05 13:21:40 +0300

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,11 +5,11 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
-    "gatsby-plugin-netlify-cms",
+    'gatsby-plugin-netlify-cms',
     `gatsby-plugin-sharp`,
 
     `gatsby-transformer-sharp`,
-    "gatsby-plugin-postcss",
+    'gatsby-plugin-postcss',
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -30,7 +30,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
   ],

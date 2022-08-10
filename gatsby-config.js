c03c9d39e9edb6a7ae7420dc4079364d7d802446
ToyEye <alexey.korotenko90@gmail.com>
2022-08-10 21:22:08 +0300

    destroy problems

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,7 +39,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `price`,
+        name: `aboutMe`,
         path: `${__dirname}/content/aboutMe`,
       },
     },

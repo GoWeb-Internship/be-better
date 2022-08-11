15a7e5fc0ddda23719408ab4f7b8adfe2507269f
shvec-es <kotova.es.1510@gmail.com>
2022-08-11 17:36:34 +0300

    add countries list and validation phone number

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,8 +3,12 @@ import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import axios from 'axios';
-
-const phoneRegEx = /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s./0-9]*$/g;
+import PhoneInput from 'react-phone-input-2';
+import 'react-phone-input-2/lib/style.css';
+import {
+  isValidPhoneNumber,
+  validatePhoneNumberLength,
+} from 'libphonenumber-js/max';
 
 const schema = yup
   .object({
@@ -17,15 +21,13 @@ const schema = yup
         'Please enter valid email'
       )
       .required('Email is required'),
-    phone: yup
-      .string()
-      .matches(phoneRegEx, 'Enter valid number')
-      .required('Phone is required'),
+    phone: yup.string().required('Phone is required'),
     checkbox: yup.boolean().required('You must accept the terms'),
   })
   .required();
 
 const Form = () => {
+  const [userLocation, setUserLocation] = React.useState('');
   const {
     register,
     control,
@@ -52,6 +54,7 @@ const Form = () => {
     Email: ${data.email}
     Phone: ${data.phone}
     Checkbox: yes
+    
     <b>Additional information:</b>
     <i>TransactionID: 11111111</i>
     <i>BlockID: 22222222</i>
@@ -72,6 +75,39 @@ const Form = () => {
       .catch(error => alert(error));
   };
 
+  axios('https://api.db-ip.com/v2/free/self')
+    .then(data => {
+      const location = data.data.countryCode;
+      setUserLocation(location.toLowerCase());
+    })
+    .catch(err => console.log(err));
+
+  const onValidatePhoneNumber = (value, country) => {
+    if (isValidPhoneNumber(value, country.iso2.toUpperCase())) {
+      return true;
+    } else if (
+      validatePhoneNumberLength(value, country.iso2.toUpperCase()) ===
+      'TOO_SHORT'
+    ) {
+      return (
+        <p className="absolute -bottom-16 -left-12 w-32">Too short number</p>
+      );
+    } else if (
+      validatePhoneNumberLength(value, country.iso2.toUpperCase()) ===
+      'TOO_LONG'
+    ) {
+      return (
+        <p className="absolute -bottom-16 -left-12 w-32">Too long number</p>
+      );
+    } else {
+      return (
+        <p className="absolute -bottom-16 -left-12 w-32">
+          Enter correct number
+        </p>
+      );
+    }
+  };
+
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
@@ -79,25 +115,37 @@ const Form = () => {
       method="POST"
       data-netlify="true"
       data-netlify-honeypot="bot-field"
+      className="w-80 m-auto"
     >
       <input type="hidden" name="form-name" value="contact-test" />
       <input
         {...register('name')}
-        className="mb-4 p-2"
+        className="w-80 mb-4 p-2"
         placeholder="Enter your name"
       />
       <p>{errors.name?.message}</p>
       <input
         {...register('email')}
-        className="mb-4 p-2"
+        className="w-80 mb-4 p-2"
         placeholder="Enter your email"
       />
       <p>{errors.email?.message}</p>
-      <input
-        {...register('phone')}
-        className="mb-4 p-2"
-        placeholder="Enter your number"
+      <Controller
+        name="phone"
+        control={control}
+        rules={{ required: true }}
+        render={({ field }) => (
+          <PhoneInput
+            country={userLocation || 'ua'}
+            placeholder="Enter phone number"
+            preferredCountries={['ua', 'gb']}
+            isValid={onValidatePhoneNumber}
+            className="mb-6"
+            {...field}
+          />
+        )}
       />
+
       <p>{errors.phone?.message}</p>
       <Controller
         name="checkbox"

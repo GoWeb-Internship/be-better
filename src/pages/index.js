6240d80c58689d8de24a5859e071afef4bc71bb9
ToyEye <alexey.korotenko90@gmail.com>
2022-08-15 16:08:44 +0300

    add readme

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,9 +17,9 @@ const IndexPage = () => {
         <Form clickFrom="footer" />
         <Social />
       </main>
-      
       <script
-dangerouslySetInnerHTML={{ __html: `
+        dangerouslySetInnerHTML={{
+          __html: `
 !function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -30,14 +30,19 @@ s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '414740170505309');
 fbq('track', 'PageView');
-`}}
-/>;
-
-<script type="text/javascript" dangerouslySetInnerHTML={{
-      __html: `
+`,
+        }}
+      />
+      ;
+      <script
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: `
       <img height="1" width="1" style="display:none"
       src="https://www.facebook.com/tr?id=414740170505309&ev=PageView&noscript=1"
-    />`}}></script>
+    />`,
+        }}
+      ></script>
     </Layout>
   );
 };

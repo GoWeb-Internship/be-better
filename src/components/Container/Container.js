fc1a89cbf85923203fa2c2a567e0000e89a87c22
ToyEye <alexey.korotenko90@gmail.com>
2022-08-17 11:00:08 +0300

    fix

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,8 +1,8 @@
 import React from 'react';
-// import {wrapper} from './Container.module.css';
+import { wrapper } from './Container.module.css';
 
 const Container = ({ children }) => {
-  return <div className="container text-center mt-4 m-auto max-w-xs h-full laptop:max-w-3xl  desktop:max-w-7xl">{children}</div>;
+  return <div className={wrapper}>{children}</div>;
 };
 
 export default Container;

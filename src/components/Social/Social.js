448e5664523992d5580e5f14a51e56228069b20b
Korotenko Alex <55750546+ToyEye@users.noreply.github.com>
2022-08-17 10:29:28 +0300

    Update Social.js

--- src/components/Social/Social.js
+++ src/components/Social/Social.js
@@ -10,62 +10,62 @@ const Social = () => {
   return (
     <div className='flex'>
     <div>
-      <Link
+      <a
         to='https://instagram.com/yuliya_shayenko?igshid=YmMyMTA2M2Y='
         target="blank"
         rel="noreferrer noopener"
         
     >
 <img src={instagram}  alt="instagram"/>
-    </Link>
+    </a>
     </div>
 
     <div>
-      <Link
+      <a
         to='https://www.facebook.com/Yuliya.Shayenko'
         target="blank"
         rel="noreferrer noopener"
         
     >
 <img src={facebook}  alt="faceebok"/>
-    </Link>
+    </a>
     </div>
 
 
 
     <div>
-       <Link
+       <a
         to='https://t.me/Petmel'
         text="hello"
         target="blank"
         rel="noreferrer noopener"
     >
 <img src={telegram}  alt="telegram"/>
-    </Link> 
+    </a> 
    
     </div>
 
 
     <div>
-      <Link
+      <a
         to="https://api.whatsapp.com/send?phone=380969019037"
         target="blank"
         rel="noreferrer noopener"
        
     >
 <img src={whatApp}  alt="whatApp"/>
-    </Link>
+    </a>
     </div>
 
     <div>
-      <Link
+      <a
         to="https://www.linkedin.com/in/yshayenko/"
         target="blank"
         rel="noreferrer noopener"
        
     >
 <img src={linkudun}  alt="linkudun"/>
-    </Link>
+    </a>
     </div>
 </div>
   );

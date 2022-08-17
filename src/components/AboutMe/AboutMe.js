7a0649304df54381f95718e7786ecf209ae529dc
ToyEye <alexey.korotenko90@gmail.com>
2022-08-17 13:24:38 +0300

    about me done

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
-import { title, container, textContainer, text } from './AboutMe.module.css';
+import AboutYou from '../AboutYou';
+import {
+  title,
+  sectionContainer,
+  textContainer,
+  text,
+} from './AboutMe.module.css';
 
 const AboutMe = () => {
   const { allMarkdownRemark } = useStaticQuery(graphql`
@@ -21,24 +27,35 @@ const AboutMe = () => {
   const [data] = allMarkdownRemark.nodes;
 
   return (
-    <section className={container}>
-      <div>
-        <StaticImage
-          src="../../images/aboutMe.jpg"
-          alt="author"
-          width={456}
-          height={480}
-          placeholder="blurred"
-          formats={['auto', 'webp']}
-        />
+    <section className={sectionContainer}>
+      <div className="flex">
+        <div>
+          <StaticImage
+            src="../../images/aboutMe.jpg"
+            alt="author"
+            width={456}
+            height={480}
+            placeholder="blurred"
+            formats={['auto', 'webp']}
+          />
+        </div>
+        <div className={textContainer}>
+          <h3 className={title}>{data.frontmatter.title}</h3>
+          <AboutYou />
+          <div
+            className={text}
+            dangerouslySetInnerHTML={{ __html: data.html }}
+          />
+        </div>
       </div>
-      <div className={textContainer}>
-        <h3 className={title}>{data.frontmatter.title}</h3>
-        <div
-          className={text}
-          dangerouslySetInnerHTML={{ __html: data.html }}
-        ></div>
-      </div>
+
+      <StaticImage
+        className="ml-auto mt-13"
+        src="../../images/signature.png"
+        width={265}
+        height={80}
+        layout="fixed"
+      />
     </section>
   );
 };

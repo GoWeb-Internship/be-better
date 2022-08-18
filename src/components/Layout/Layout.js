1b3a5561c54e84cb69818f53e05aeb64fba4f1fc
ToyEye <alexey.korotenko90@gmail.com>
2022-08-18 17:40:31 +0300

    fix

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,11 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import { MDXProvider } from '@mdx-js/react';
 import { useStaticQuery, graphql } from 'gatsby';
 import Footer from '../Footer';
-import { IoIosArrowRoundDown } from 'react-icons/io';
 import Header from '../Header';
 import Arrow from '../ArrowSvg';
 
 const Layout = ({ children }) => {
-  const shortcodes = { Arrow, IoIosArrowRoundDown };
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -22,7 +19,6 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <MDXProvider components={shortcodes}>{children}</MDXProvider>
       <main className="container m-auto">{children}</main>
       <Footer />
     </>

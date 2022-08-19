3c148ef54463270b1f66afc919aba6037ddcde2c
ToyEye <alexey.korotenko90@gmail.com>
2022-08-19 09:56:07 +0300

    add

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import Form from '../Form';
 import Social from '../Social';
 import FooterNext from '../FooterNext';
 import { StaticImage } from 'gatsby-plugin-image';
-import { footerContainer } from './Footer.module.css';
+import { footerContainer, list, link, container } from './Footer.module.css';
 
 const Footer = () => {
   const { t } = useTranslation();
@@ -14,49 +14,56 @@ const Footer = () => {
   return (
     <footer className={footerContainer}>
       {/* <Container className="m-0 p-0 w-full flex relative"> */}
-      <div className="flex justify-between m-0 p-0 w-full relative">
+      <div className={container}>
+        {/* <StaticImage
+      alt=''
+      formats={['auto', 'webp', 'avif']}
+      src='../../images/bg-min.png'
+      width={1031}
+      height={420}
+      className="absolute -z-10"
+      /> */}
         <StaticImage
+          layout="fullWidth"
+          src="../../images/background/bg-min.png"
           alt=""
-          src="../../images/bg-min.png"
-          width={1100}
-          height={420}
-          className="absolute m-0 -z-10"
+          style={{ position: 'absolute' }}
+          className="w-3/4 -z-10 top-0"
         />
-        <div className="pt-16 pl-12 pr-14">
-          <StaticImage
-            src="../../images/logo.png"
-            width={90}
-            height={56}
-            layout="fixed"
-            className="mb-11"
-            alt=""
-          />
-          <StaticImage
-            src="../../images/icf.png"
-            width={96}
-            height={96}
-            layout="fixed"
-            alt=""
-          />
+        <div className="flex">
+          <div className="pt-16 pl-12 pr-14">
+            <StaticImage
+              src="../../images/logo.png"
+              width={90}
+              height={56}
+              layout="fixed"
+              className="mb-11"
+            />
+            <StaticImage
+              src="../../images/icf.png"
+              width={96}
+              height={96}
+              layout="fixed"
+            />
+          </div>
+
+          <FooterNext className="absolute" />
+
+          <Form clickFrom="footer" className="-mb-4" />
+          <Social className="z-3 pt-16 absolute" />
         </div>
-
-        <FooterNext />
-
-        <Form clickFrom="footer" className="pt-16" />
-        <Social className="z-3 pt-16" />
+        <div>
+          <ul className={list}>
+            {links.map(({ name, id }) => (
+              <li key={id} className="last: ml-5 z-10">
+                <Link className={link} to={`/${id}`}>
+                  {name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <ul className="flex text-left pl-44 ml-10 mr-4 z-10">
-        {links.map(({ name, id }) => (
-          <li key={id} className="last: ml-5 z-10">
-            <Link
-              className="pr-7 text-black font-normal text-sm  px-0"
-              to={`/${id}`}
-            >
-              {name}
-            </Link>
-          </li>
-        ))}
-      </ul>
 
       {/* </Container> */}
     </footer>

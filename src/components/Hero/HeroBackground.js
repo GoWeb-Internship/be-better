bf55a395a828555a043ad7d25dcad14436bee306
shvec-es <kotova.es.1510@gmail.com>
2022-08-22 08:45:16 +0300

    add guarantee styles

--- src/components/Hero/HeroBackground.js
+++ src/components/Hero/HeroBackground.js
@@ -9,7 +9,7 @@ const HeroBackground = () => {
         src="../../images/background/heroDesktop.png"
         alt=""
         style={{ position: 'absolute' }}
-        className="w-[1036px] h-full -z-10 top-0 right-0"
+        className="w-1036 h-full -z-10 top-0 right-0"
       />
     </div>
   );

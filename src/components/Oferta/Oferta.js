9ec61df425385f446a7ef3bdc4a903e7d7bae3a2
MariiaMelychyn <lv080191mmi75@gmail.com>
2022-08-23 08:04:27 +0300

    Update Oferta.js

--- src/components/Oferta/Oferta.js
+++ src/components/Oferta/Oferta.js
@@ -28,21 +28,23 @@ const data = allMarkdownRemark.nodes;
     <div className='relative'>
          <StaticImage
                   layout="fullWidth"
-                  src="../../images/background/ofertal-min.png"
-                  alt=""
-                  style={{ position: 'absolute' }}
-                  className="float-right w-3/4 -z-10 top-0  max-w-md  mt-14 mb-16  h-60 rounded-lg "
-                  formats={['auto', 'webp', 'avif']}
-                />
-         <StaticImage
-                  layout="fullWidth"
                   src="../../images/background/ofertar-min.png"
                   alt=""
                   style={{ position: 'absolute' }}
                   className="w-3/4 -z-20 top-0 ml-80 mb-16 pr-0 max-w-5xl"
                   formats={['auto', 'webp', 'avif']}
                 />
-    <div className="text-start pl-72">
+              
+                 <StaticImage
+                  layout="fullWidth"
+                  src="../../images/background/ofertal-min.png"
+                  alt=""
+                  style=""
+                  className="w-3/4 -z-10 top-0  max-w-md  !float-right mr-40  mt-14 mb-16  h-60 rounded-lg "
+                  formats={['auto', 'webp', 'avif']}
+                />
+              
+    <div className="text-start tablet:pl-5 laptop:pl-28 desktop:pl-72">
       {data.map(node => {
         if (node.frontmatter.language === i18n.language) {
           return (

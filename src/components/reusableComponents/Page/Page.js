9af95ce9b730fb6f9e11cf02ce4c7082691d981a
MariiaMelychyn <lv080191mmi75@gmail.com>
2022-08-24 18:53:27 +0300

    steps

--- src/components/reusableComponents/Page/Page.js
+++ src/components/reusableComponents/Page/Page.js
@@ -11,25 +11,28 @@ const Page = ({ icons = '', data = {}, img ='', icon='' }) => {
           {data.page.map(({ textPrimary, svg, img, icon, textSecondary }) => {
             return (
               <li className={itemFacts} key={icons}>
+  <div><p className={textPr}>{textPrimary}</p></div>
+
   <div className={iconClass}>
-                <svg className={icons}>
-                    <use  href={`${icons}#icon-${icon}`} />
+                <svg className="w-10 h-10 -mt-20 ml-6">
+                    <use className='w-10 h-10 -mt-40'  href={`${icons}#icon-${icon}`} />
                   </svg>
                 </div> 
+
                 <div className={svgContainerFacts}>
                   <svg className={iconFacts}>
                     <use href={`${icons}#icon-${img}`} />
                   </svg>
-                  <p className={textPr}>{textPrimary}</p>
+                  
                 </div>
                 
-                <div className={textContainer}>
+                {/* <div className={textContainer}>
                  
                  
                   <svg className={svgBgFacts}>
                     <use href={`${icons}#icon-${svg}`} />
                   </svg>
-                </div>
+                </div> */}
                   <p className={testSec}>{textSecondary}</p>
                 
               </li>

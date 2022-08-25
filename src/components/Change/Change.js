3046239c3848c24d4ad0f14c19207422df6ab2f3
MariiaMelychyn <lv080191mmi75@gmail.com>
2022-08-25 20:13:06 +0300

    tablet

--- src/components/Change/Change.js
+++ src/components/Change/Change.js
@@ -63,7 +63,7 @@ const Change = () => {
   };
 
   return (
-    <div>
+    <div className='tablet:-mt-[57%] laptop:-mt-[17%] desktop:-mt-[11%]'>
       {data.map(node => {
         if (node.frontmatter.language === i18n.language) {
           return (
@@ -74,7 +74,7 @@ const Change = () => {
                   src="../../images/background/fon-min.png"
                   alt=""
                   style={{ position: 'absolute' }}
-                  className="tablet:w-[47%] tablet:ml-40 tablet:pb-10 -z-10 mt-5 laptop:-mt-8 laptop:h-28 laptop:w-[79%] ml-80 mb-16 pr-0 desktop:top-0 desktop:mt-0 desktop:w-3/4 max-w-5xl"
+                  className="tablet:w-[47%] tablet:ml-40 tablet:pb-10 -z-10 mt-5 laptop:-mt-8 laptop:h-28 laptop:w-[79%] desktop:w-[100%] desktop:ml-[29%] ml-80 mb-16 pr-0 desktop:top-0 desktop:mt-0  max-w-5xl"
                   formats={['auto', 'webp', 'avif']}
                 />
                 <StaticImage

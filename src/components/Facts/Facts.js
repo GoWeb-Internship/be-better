83c4305dddb13774a6b8b7f61a87f0194576dac5
ToyEye <alexey.korotenko90@gmail.com>
2022-08-26 10:13:05 +0300

    fix foto

--- src/components/Facts/Facts.js
+++ src/components/Facts/Facts.js
@@ -5,16 +5,23 @@ import { StaticImage } from 'gatsby-plugin-image';
 import List from '../reusableComponents/List';
 import svg from '../../images/factsIcons.svg';
 import Way from '../Way';
+import {
+  section,
+  title,
+  contentContainer,
+  imgMob,
+  img,
+} from './Facts.module.css';
+import { useMedia } from 'react-use';
 
 const Facts = () => {
   const { t } = useTranslation();
   const data = t('facts', { returnObjects: true });
+  const isWide = useMedia('(min-width:1440px');
 
   return (
-    <Section className="relative py-8   laptop:pt-12 laptop:pb-[104px] desktop:pt-20 desktop:pb-16 ">
-      <h3 className="title-secondary mb-4 laptop:mb-8 desktop:hidden laptop:text-34">
-        {data.title}
-      </h3>
+    <Section className={section}>
+      <h3 className={title}>{data.title}</h3>
 
       <StaticImage
         layout="fullWidth"
@@ -23,28 +30,30 @@ const Facts = () => {
         style={{ position: 'absolute' }}
         className="w-full h-full -z-10 top-0"
       />
-      <div className="flex flex-col-reverse desktop:flex-row px-5 laptop:px-16  desktop:px-46 ">
+      <div className={contentContainer}>
         <List data={data} icons={svg} />
 
-        <div className="desktop:hidden ">
-          <StaticImage
-            alt="foto"
-            src="../../images/factMob.jpg"
-            className="rounded-2xl desktop:ml-13 w-70 h-[200px] laptop:w-[640px] laptop:h-[440px] "
-            placeholder="blurred"
-            formats={['auto', 'webp', 'avif']}
-          />
-        </div>
-
-        <div className="hidden desktop:block">
-          <StaticImage
-            alt="foto"
-            src="../../images/factsFoto.jpg"
-            className="rounded-2xl desktop:ml-13  desktop:w-[380px] desktop:h-[560px] "
-            placeholder="blurred"
-            formats={['auto', 'webp', 'avif']}
-          />
-        </div>
+        {isWide ? (
+          <div className="hidden desktop:block">
+            <StaticImage
+              alt="foto"
+              src="../../images/factsFoto.jpg"
+              className={img}
+              placeholder="blurred"
+              formats={['auto', 'webp', 'avif']}
+            />
+          </div>
+        ) : (
+          <div className="desktop:hidden ">
+            <StaticImage
+              alt="foto"
+              src="../../images/factMob.jpg"
+              className={imgMob}
+              placeholder="blurred"
+              formats={['auto', 'webp', 'avif']}
+            />
+          </div>
+        )}
       </div>
       <Way />
     </Section>

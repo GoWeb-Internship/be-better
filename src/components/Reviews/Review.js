803a9c3f6564b77cbe5352c9d758008f9c9a203f
ToyEye <alexey.korotenko90@gmail.com>
2022-08-26 18:28:54 +0300

    review

--- src/components/Reviews/Review.js
+++ src/components/Reviews/Review.js
@@ -1,58 +1,68 @@
 import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { BsPerson } from 'react-icons/bs';
-import {
-  reviewContainer,
-  textContainer,
-  buttonCss,
-} from './Reviews.module.css';
-import Button from '../reusableComponents/Button';
+import { reviewContainer, textContainer } from './Reviews.module.css';
+import { useMedia } from 'react-use';
+import { use } from 'i18next';
 
 const Review = ({ frontmatter }) => {
-  const [showAllReview, setShowAllReview] = useState(false);
   const [overflow, setOverflow] = useState('overflow-hidden');
+  const [limitText, setLimitText] = useState(200);
+
+  const { i18n } = useTranslation();
+
+  const isMobile = useMedia('(max-width:767px)');
+
+  const isDesktop = useMedia('(min-width:1440px)');
 
   useEffect(() => {
-    if (showAllReview) {
-      setOverflow('overflow-auto');
-      return;
+    const showScroll = () => {
+      const contentText = frontmatter[`${i18n.language}Text`].length;
+
+      if (contentText > limitText) {
+        setOverflow('overflow-y-auto');
+      } else {
+        setOverflow('overflow-hidden');
+      }
+    };
+    showScroll();
+  }, [overflow, limitText]);
+
+  useEffect(() => {
+    if (isMobile) {
+      setLimitText(150);
+    } else if (isDesktop) {
+      setLimitText(220);
     } else {
-      setOverflow('overflow-hidden');
-      return;
+      setLimitText(180);
     }
-  }, [showAllReview]);
+  }, [limitText, isMobile, isDesktop]);
 
-  const { t, i18n } = useTranslation();
+  const positionLength = frontmatter[`${i18n.language}Position`];
 
-  const showMore = () => {
-    setShowAllReview(!showAllReview);
-  };
-  const button = t('littleComponents', { returnObjects: true });
+  const position = positionLength === null ? 'mb-6' : 'opacity-0';
+
   return (
     <div className={reviewContainer}>
-      <div className="flex flex-col justify-between h-full">
-        <div className={`${overflow} ${textContainer}`}>
-          <div>
-            <p>{frontmatter[`${i18n.language}Text`]}</p>
-          </div>
-        </div>
-        <div className="flex items-center">
-          <div className="rounded-full mr-6 ">
-            <BsPerson size={24} />
-          </div>
-          <div>
-            <p className="  laptop:text-xl font-bold">
-              {frontmatter[`${i18n.language}Name`]}
-            </p>
-            <p>{frontmatter[`${i18n.language}Position`]}</p>
-          </div>
+      <div className={`${overflow} ${textContainer} h-28  laptop:h-[200px] `}>
+        <div>
+          <p>{frontmatter[`${i18n.language}Text`]}</p>
         </div>
       </div>
-      {!showAllReview && (
-        <Button doAction={showMore} className={buttonCss}>
-          <p className="!text-mainSecond">{button.reviewButtonMore}</p>
-        </Button>
-      )}
+
+      <div className="flex items-center">
+        <div className="rounded-full mr-6 ">
+          <BsPerson size={24} />
+        </div>
+        <div>
+          <p className={`{${position}} laptop:text-xl font-bold`}>
+            {frontmatter[`${i18n.language}Name`]}
+          </p>
+          <p className={`h-4 laptop:h-6 text-orangeDirty`}>
+            {frontmatter[`${i18n.language}Position`]}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };

3d8c8ac12c0c9594bceb4ec32514479061dbab8f
MariiaMelychyn <lv080191mmi75@gmail.com>
2022-08-27 23:17:48 +0300

    steps

--- src/components/WhyStepsAdd/WhyStepsAdd.js
+++ src/components/WhyStepsAdd/WhyStepsAdd.js
@@ -20,7 +20,7 @@ const StepsAdd = () => {
           style={{ position: 'absolute' }}
           className="w-[416px] h-[128px] tablet:hidden laptop:block  laptop:left-1/4 z-1 laptop:top-[45%]  desktop:pl-40 desktop:left-2/4 desktop:top-[35%] "
         />
-
+       <div className='tablet:hidden laptop:block'>
         <StaticImage
           layout="fullWidth"
           src="../../images/background/features.jpg"
@@ -28,6 +28,16 @@ const StepsAdd = () => {
           style={{ position: 'absolute' }}
           className="-z-20 w-full h-full"
         />
+        </div>
+        <div className='laptop:hidden'>
+        <StaticImage
+          layout="fullWidth"
+          src="../../images/background/vectwo-min.png"
+          alt=""
+          style={{ position: 'absolute' }}
+          className="-z-20 w-full h-full"
+        />
+        </div>
         <List data={data} icons={svg} />
         <Reviews />
       </div>

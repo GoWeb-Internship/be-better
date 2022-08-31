7f81c1a4d393f10dc9e78b8bcd08fa5bd6a36e03
ToyEye <alexey.korotenko90@gmail.com>
2022-08-31 11:57:18 +0300

    little fix

--- src/components/seo.js
+++ src/components/seo.js
@@ -22,14 +22,11 @@ function Seo({ description, title, children }) {
   return (
     <>
       <title>{defaultTitle ? `${title} | ${defaultTitle}` : title}</title>
+      <meta charSet="utf-8" />
       <meta name="description" content={metaDescription} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={metaDescription} />
       <meta property="og:type" content="website" />
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:creator" content={site.siteMetadata?.author || ``} />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={metaDescription} />
       {children}
     </>
   );

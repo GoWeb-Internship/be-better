d5be421b751dce17b716f09fcf517c495f77ab26
MariiaMelychyn <lv080191mmi75@gmail.com>
2022-09-05 12:48:50 +0300

    remove gatsby image

--- src/components/Steps/Steps.js
+++ src/components/Steps/Steps.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { graphql, useStaticQuery } from 'gatsby';
-import { StaticImage, GatsbyImage } from 'gatsby-plugin-image';
+import { StaticImage } from 'gatsby-plugin-image';
 import Section from '../reusableComponents/Section';
 import List from '../reusableComponents/Page';
 import { useTranslation } from 'gatsby-plugin-react-i18next';
@@ -10,57 +9,9 @@ const Steps = () => {
   const { t } = useTranslation();
   const data = t('steps', { returnObjects: true });
 
-  const foto = useStaticQuery(graphql`
-  query {
-    avatarVanMin: file(name: { eq: "vanMin" }) {
-      id
-      publicURL
-      childImageSharp {
-        id
-        gatsbyImageData(
-          placeholder: BLURRED
-          formats: [AUTO, WEBP, AVIF]
-          layout: CONSTRAINED
-        )
-      }
-    }
-    avatarResults: file(name: { eq: "results" }) {
-      id
-      publicURL
-      childImageSharp {
-        id
-        gatsbyImageData(
-          placeholder: BLURRED
-          formats: [AUTO, WEBP, AVIF]
-          layout: CONSTRAINED
-        )
-      }
-    }
-    avatarGroup: file(name: { eq: "group" }) {
-      id
-      publicURL
-      childImageSharp {
-        id
-        gatsbyImageData(
-          placeholder: BLURRED
-          formats: [AUTO, WEBP, AVIF]
-          layout: CONSTRAINED
-        )
-      }
-    }
-  }
-  
-`);
-
-const avatarVan = foto.avatarVanMin.childImageSharp.gatsbyImageData;
-const avatarRes = foto.avatarResults.childImageSharp.gatsbyImageData;
-const avatarGr = foto.avatarGroup.childImageSharp.gatsbyImageData;
-
-
- 
 
   return (
-    <Section className="pt-20 pb-16  relative" id="steps">
+    <Section className="pt-20 laptop:pb-16  relative" id="steps">
       <div className="relative">
         <div className="relative max-h-full">
           <div className="tablet:hidden desktop:block laptop:w-[768px] ">
@@ -72,14 +23,6 @@ const avatarGr = foto.avatarGroup.childImageSharp.gatsbyImageData;
               className="-z-20 w-full h-full"
             />
           </div>
-          {/* <div className="tablet:hidden desktop:block laptop:w-[768px] pl-[0px] -ml-[384px] pr-0 mr-0">
-          <GatsbyImage
-              image={avatarRes}
-              alt="result"
-              style={{ position: 'absolute' }}
-              className="-z-20 w-full h-full"
-            />
-          </div> */}
           <div className="tablet:hidden laptop:block desktop:hidden laptop:w-[768px] ">
             <StaticImage
               layout="fullWidth"
@@ -89,15 +32,6 @@ const avatarGr = foto.avatarGroup.childImageSharp.gatsbyImageData;
               className="-z-20 w-full h-full"
             />
           </div>
-          {/* <div className="tablet:hidden laptop:block desktop:hidden laptop:w-[768px] -ml-[383px]">
-          <GatsbyImage
-              image={avatarGr}
-              alt="result"
-              style={{ position: 'absolute' }}
-              className="-z-20 w-full h-full"
-            />
-          </div> */}
-
            <div className="laptop:hidden">
             <StaticImage
               layout="fullWidth"
@@ -107,14 +41,6 @@ const avatarGr = foto.avatarGroup.childImageSharp.gatsbyImageData;
               className="-z-20 w-full h-full"
             />
            </div> 
-            {/* <div className="-ml-[335px] laptop:hidden">
-             <GatsbyImage
-              image={avatarVan}
-              alt="avatar"
-              style={{ position: 'absolute' }}
-              className="-z-20 w-full h-full"
-            />
-           </div> */}
           <List data={data} icons={svg} />
         </div>
       </div>

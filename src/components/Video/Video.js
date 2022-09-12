6a6d8f8fd75c854ccfaabb494a911b3a32209131
Evgeniya Shvec <82171958+shvec-es@users.noreply.github.com>
2022-09-12 15:30:21 +0300

    fix big

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import {video} from './Video.module.css';
 import beach from '../../images/video/beach.mp4';
 
 const Video = () => {
   return (
-    // <div className={video}>
       <video autoPlay loop muted playsInline className="w-[1036px] absolute -z-10 top-0 left-0">
         <source src={beach} type="video/mp4" />
         <source src={beach} type="video/ogg" />
       </video>
-    // </div>
   );
 };
 

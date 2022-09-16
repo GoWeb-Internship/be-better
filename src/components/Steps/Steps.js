d66f0a536b1ba8f4ad7e5a69cd21205dcd817bae
ToyEye <alexey.korotenko90@gmail.com>
2022-09-16 13:16:14 +0300

    fix

--- src/components/Steps/Steps.js
+++ src/components/Steps/Steps.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useTranslation } from 'gatsby-plugin-react-i18next';
 import { StaticImage } from 'gatsby-plugin-image';
 
+import useMediaRules from 'helpers/getMedia';
 import Section from 'components/reusableComponents/Section';
 import List from 'components/reusableComponents/Page';
 
@@ -10,11 +11,12 @@ import svg from 'images/iconku.svg';
 const Steps = () => {
   const { t } = useTranslation();
   const data = t('steps', { returnObjects: true });
+  const media = useMediaRules();
 
   return (
     <Section className="laptop:pb-16" id="steps">
       <div className="relative max-h-full">
-        <div className="tablet:hidden desktop:block laptop:w-[768px] ">
+        {media === 'desktop' && (
           <StaticImage
             layout="fullWidth"
             src="../../images/results.png"
@@ -22,8 +24,17 @@ const Steps = () => {
             style={{ position: 'absolute' }}
             className="-z-20 w-full h-full"
           />
-        </div>
-        <div className="tablet:hidden laptop:block desktop:hidden laptop:w-[768px] ">
+        )}
+        {/* <div className="tablet:hidden desktop:block laptop:w-[768px] ">
+          <StaticImage
+            layout="fullWidth"
+            src="../../images/results.png"
+            alt={data.background}
+            style={{ position: 'absolute' }}
+            className="-z-20 w-full h-full"
+          />
+        </div> */}
+        {media === 'tablet' && (
           <StaticImage
             layout="fullWidth"
             src="../../images/group.png"
@@ -31,8 +42,17 @@ const Steps = () => {
             style={{ position: 'absolute' }}
             className="-z-20 w-full h-full"
           />
-        </div>
-        <div className="laptop:hidden">
+        )}
+        {/* <div className="tablet:hidden laptop:block desktop:hidden laptop:w-[768px] ">
+          <StaticImage
+            layout="fullWidth"
+            src="../../images/group.png"
+            alt={data.background}
+            style={{ position: 'absolute' }}
+            className="-z-20 w-full h-full"
+          />
+        </div> */}
+        {media === 'mobile' && (
           <StaticImage
             layout="fullWidth"
             src="../../images/vanMin.png"
@@ -40,7 +60,16 @@ const Steps = () => {
             style={{ position: 'absolute' }}
             className="-z-20 w-full h-full"
           />
-        </div>
+        )}
+        {/* <div className="laptop:hidden">
+          <StaticImage
+            layout="fullWidth"
+            src="../../images/vanMin.png"
+            alt={data.background}
+            style={{ position: 'absolute' }}
+            className="-z-20 w-full h-full"
+          />
+        </div> */}
         <List data={data} icons={svg} />
       </div>
     </Section>

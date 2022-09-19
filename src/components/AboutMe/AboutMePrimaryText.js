794f267559b34da4fb5df4e94401d26c70332f77
ToyEye <alexey.korotenko90@gmail.com>
2022-09-19 18:39:21 +0300

    fix

--- src/components/AboutMe/AboutMePrimaryText.js
+++ src/components/AboutMe/AboutMePrimaryText.js
@@ -3,18 +3,14 @@ import PropTypes from 'prop-types';
 
 import Heading from 'components/reusableComponents/Heading';
 import AboutYou from 'components/AboutYou';
-import useMediaRules from 'helpers/getMedia';
 
 import { title, textContainer, caveat } from './AboutMe.module.css';
 
 const AboutMePrimaryText = ({ data }) => {
-  const media = useMediaRules();
-
   return (
     <div className={textContainer}>
-      {media !== 'mobile' && (
-        <Heading tag="h2" className={title} text={data.title} />
-      )}
+      <Heading tag="h2" className={title} text={data.title} />
+
       <p className="mb-2 desktop:mb-8">
         <span className="text-black hidden laptop:inline">
           {data.oneParagraphFirst}
@@ -26,19 +22,18 @@ const AboutMePrimaryText = ({ data }) => {
         <span className={caveat}> {data.twoParagraphSpan}</span>
         {data.twoParagraphThird}
       </p>
-      {media !== 'tablet' && (
-        <>
-          <p className="mb-2 desktop:mb-8">
-            {data.threeParagraphFirst}
-            <span className={caveat}>{data.threeParagraphSpan}</span>
-            {data.threeParagraphThird}
-          </p>
-          <p className="mb-6 laptop:mb-0">{data.fourParagraph}</p>
-          <p className="mb-4 laptop:mb-0">
-            <span className="text-caveat">{data.fiveParagraphSpan}</span>
-          </p>
-        </>
-      )}
+
+      <div className="laptop:hidden desktop:block">
+        <p className=" mb-2 desktop:mb-8">
+          {data.threeParagraphFirst}
+          <span className={caveat}>{data.threeParagraphSpan}</span>
+          {data.threeParagraphThird}
+        </p>
+        <p className="mb-6 laptop:mb-0">{data.fourParagraph}</p>
+        <p className="mb-4 laptop:mb-0">
+          <span className="text-caveat">{data.fiveParagraphSpan}</span>
+        </p>
+      </div>
       <AboutYou />
     </div>
   );

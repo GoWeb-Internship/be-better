f4c4e19382e41f3f5fd304553ff4d1ceafdcc41b
Evgeniya Shvec <82171958+shvec-es@users.noreply.github.com>
2022-09-20 12:43:50 +0300

    remove outline-none

--- src/components/Form/JustForm.js
+++ src/components/Form/JustForm.js
@@ -170,7 +170,6 @@ const JustForm = ({
               enableSearch="true"
               searchPlaceholder="Choose country"
               disableSearchIcon="true"
-              containerClass={'outline-none'}
               buttonClass={phonebutton}
               inputClass={phoneinput}
               dropdownClass={phonedropdown}
